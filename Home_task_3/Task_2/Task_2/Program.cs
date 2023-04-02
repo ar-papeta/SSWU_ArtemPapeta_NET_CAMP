9925411c0c15430157479b4602910694a6f37816
Artem Papeta <ar.papeta@gmail.com>
2023-04-02 16:12:12 +0300

    Hometask_3: complete task 2

---
+++ Home_task_3/Task_2/Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Task_2;
+
+Console.WriteLine("Enter string");
+string? userInput = Console.ReadLine();
+
+Console.WriteLine("Enter substring");
+string? subString = Console.ReadLine();
+
+Console.WriteLine($"\nSecond index of \"{subString}\" is {userInput!.SecondIndexOf(subString!)}");
+Console.WriteLine($"Count words start with capital letter is {userInput!.FirstCapitalLetterWordCount()}");
+userInput = userInput!.SwapDoubleLettersTo(subString!);
+Console.WriteLine($"New string with swaped double letters to \"{subString}\": \n{userInput!}");
+
+
---
+++ Home_task_3/Task_2/Task_2/StringExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2;
+
+internal static class StringExt
+{
+    public static int FirstCapitalLetterWordCount(this string input) => 
+        input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(s => !string.IsNullOrEmpty(s) && char.IsUpper(s[0])).Count();
+    
+
+    public static int? SecondIndexOf(this string input, string subString)
+    {
+        var index = input.IndexOf(subString, input.IndexOf(subString) + 1);
+
+        return index == -1 ? null : index;
+    }
+
+    public static string SwapDoubleLettersTo(this string input, string text)
+    {
+        return input.Split(' ')
+                .Select(x =>
+                    x.Where((c, i) => i > 0 && c == x[i - 1]).Cast<char?>().FirstOrDefault() is not null ? x.Replace(x, text) : x)
+                .Aggregate((s1, s2) => String.Format("{0} {1}", s1, s2)); ;
+    }
+}

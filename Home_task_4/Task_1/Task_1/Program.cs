c4480afea5ebd5b461db9d34a961ea3f1fbd070c
Artem Papeta <ar.papeta@gmail.com>
2023-04-12 23:44:49 +0300

    Complete task 4.1

---
+++ Home_task_4/Task_1/Task_1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+using System.Text;
+using Task_1;
+
+//CultureInfo.CurrentCulture = new CultureInfo("uk-UA");
+Console.OutputEncoding = Encoding.UTF8;
+
+List<string> stringsEn = new()
+{
+    "A(d)daa.",
+    "Asdasd< sfsdfsd safdas",
+    "Sasd (asd) asd!",
+    "(asd asd,",
+    "asd wqer rqr nhgj)",
+    "123 445 gasg?",
+    "213vfertgf tergteqr qqrewq (eqwe fasd).",
+};
+
+List<string> stringsUa = new()
+{
+    "фівафві.",
+    "йукук укейуц цйукй цуйк",
+    "йцкуйцу йцукйук йцук (фіавфі)!",
+    "(йцкцйукб цйукб  , цукц     ",
+    "йуцк йцку йцук фівафіва)",
+    "123 445 12344?",
+    "1йкуй фіовл фіугцйш щйщ (фіваіф віаів) іваіва іваю.",
+};
+
+SentenceFinder finder = new(stringsUa);
+try
+{
+    var strings = finder.FindSentencesWithBrackets();
+    foreach (var item in strings)
+    {
+        Console.WriteLine(item);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.WriteLine("-------------------");
+
+SentenceFinder finder2 = new(stringsEn);
+try
+{
+    var strings2 = finder2.FindSentencesWithBrackets();
+    foreach (var item in strings2)
+    {
+        Console.WriteLine(item);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
---
+++ Home_task_4/Task_1/Task_1/SentenceFinder.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Globalization;
+
+namespace Task_1;
+public enum Culture
+{
+    uk_UA = 0,
+    en_US = 1,
+}
+internal class SentenceFinder
+{
+    private readonly List<string> _lines;
+    private readonly char[] _textChars;
+    private readonly char[] _sentenceSeparators = new char[] { '!', '.', '?'};
+    private readonly char[] _openChars = new char[] { '(', '[', '{' };
+    private readonly char[] _closedChars = new char[] { ')', ']', '}' };
+
+    public SentenceFinder(List<string> lines)
+    {
+        _lines = lines;
+        _textChars = _lines.Select(s => s.ToCharArray()).Aggregate((s1, s2) => s1.Concat(s2).ToArray());
+    }
+    public SentenceFinder(List<string> lines, char[] sentenceSeparators, char[] openChars, char[] closedChars)
+    {
+        _closedChars = closedChars;
+        _sentenceSeparators = sentenceSeparators;
+        _openChars = openChars;
+        _lines = lines;
+        _textChars = _lines.Select(s => s.ToCharArray()).Aggregate((s1, s2) => s1.Concat(s2).ToArray());
+    }
+
+    public List<string> FindSentencesWithBrackets()
+    {
+        int lastStartIndex = 0;
+        int lastEndIndex = 0;
+        List<string> strings = new();
+        for (int i = 0; i < _textChars.Length; i++)
+        {
+            if (_openChars.Contains(_textChars[i]))
+            {
+                lastStartIndex = Array.FindIndex(_textChars, lastEndIndex, i - lastEndIndex, x => _sentenceSeparators.Contains(x));
+
+                lastEndIndex = Array.FindIndex(_textChars, i, x => _sentenceSeparators.Contains(x));
+                string sentence = new string(_textChars[(lastStartIndex + 1)..(lastEndIndex + 1)]).Trim();
+
+                if(sentence.IndexOfAny(_closedChars) == -1)
+                {
+                    throw new Exception("Sentence have no closed char");
+                }
+                strings.Add(sentence);
+            }
+            //"Вважаємо що дужки в тексті розташовані правильно і немає вкладеності."
+            //Тому не бачу алгоритмічного сенсу перевіряти чи закривається відкрита дужка в реченні
+            //(завжди приймаю що так, згідно з вищецитованою стрічкою вашого ТЗ)
+            //Крім того немає опису кейсу, що робити якщо дужка не закрилася до кінця речення 
+            //Але все-таки добавлю перевірку :)
+        }
+        return strings;
+    }
+}

3090d572ac46484b55c048952229f57ddcf905ae
Artem Papeta <ar.papeta@gmail.com>
2023-04-15 19:21:29 +0300

    Task_2 : Implementation of checks

---
+++ Home_task_4/Task_2/Task_2/EmailChecker.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2;
+
+public enum CheckResult
+{
+    Success,
+    
+}
+
+internal class EmailChecker
+{
+    private string _emailCandidate;
+    private string _localPart;
+    private bool _isLocalInQuotation;
+    private string _domainPart;
+
+    private char[] _allowedCharacters = { '!', '#', '$', '%', '&', '\'', '*', '+', '-', '/', '=', '?', '^', '_', '`', '{', '|', '}', '~'};
+    public EmailChecker(string emailCandidate)
+    {
+        _emailCandidate = emailCandidate;
+    }
+
+    public bool IsCorrectMail()
+    {
+        if(_emailCandidate is null)
+        {
+            return false;
+        }
+
+        RemoveCommentsFromEmailCheck();
+
+        SplitToLocalAndDomain();
+
+        bool bl = LocalPartChecks();
+
+        DomainPartChecks();
+
+        return true; 
+    }
+
+    private bool LocalPartChecks()
+    {
+        if (IsLocalCorrectQuoted())
+        {
+            return _localPart[1..(_localPart.Length - 2)].IndexOfAny(new char[] {'"', '\\'}) == -1;
+        }
+
+        foreach (var c in _localPart)
+        {
+            if(char.IsAsciiLetterOrDigit(c) ||  )
+        }
+        return true;
+    }
+
+    
+
+    private bool DomainPartChecks()
+    {
+        return true;
+    }
+
+    private bool IsLocalCorrectQuoted()
+    {
+        var count = _localPart.Count(c => c == '"');
+
+        return (count == 2) && _localPart.IndexOf('"') == 0 && _localPart.LastIndexOf('"') == _localPart.Length-1;
+        
+    }
+    private void RemoveCommentsFromEmailCheck()
+    {
+        var openIndex = _emailCandidate.IndexOf('(');
+
+        if(openIndex == -1)
+        {
+            return;
+        }
+        var commentCount = _emailCandidate.Count(c => c == ')');
+        for (int i = 0; i < commentCount; i++)
+        {
+            var closedIndex = _emailCandidate.IndexOf(')');
+            _emailCandidate = _emailCandidate[..openIndex] + _emailCandidate[(closedIndex + 1)..];
+            openIndex = _emailCandidate.IndexOf('(');
+
+            if(openIndex == -1 && commentCount != i + 1)
+            {
+                throw new ValidationException("Email have wrong format coments");
+            }
+        }
+    }
+
+    private void SplitToLocalAndDomain()
+    {
+
+        var separatorIndex = _emailCandidate.LastIndexOf('@');
+        if(separatorIndex == -1)
+        {
+            throw new ValidationException("Email have noone @");
+        }
+        _localPart = _emailCandidate[..separatorIndex];
+        _domainPart = _emailCandidate[(separatorIndex + 1)..];
+    }
+}
---
+++ Home_task_4/Task_2/Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task_2;
+
+Console.WriteLine("Hello, World!");
+
+string EmailText =
+    //valid
+    "simple@example.com"
+    + '\n'
+    + "disposable.style.email.with+symbol@example.com"
+    + '\n'
+    + "example-indeed@strange-example.com"
+    + '\n'
+    + "\" \"@example.org"
+    + '\n'
+    + "\"john..doe\"@example.org"
+    + '\n'
+    + "mailhost!username@example.org"
+    + '\n'
+    + "admin@mailserver1"
+    + '\n'
+    + "(this is ( comment in email)simple@example.com(domain comment)"
+    + '\n'
+    + '\n'
+    + '\n'
+    //invalid
+    + "Abc.example.com"
+    + '\n'
+    + "A@b@c@example.com"
+    + '\n'
+    + "i_like_underscore@but_its_not_allow_in _this_part.example.com"
+    + '\n'
+    + "just\"not\"right@example.com";
+
+string s = "\"simple\"@example.com";
+EmailChecker checker = new EmailChecker(s);
+checker.IsCorrectMail();

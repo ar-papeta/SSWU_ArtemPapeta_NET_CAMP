76d6dbe7166a236de3ec4bea3224dae46395f281
Artem Papeta <ar.papeta@gmail.com>
2023-04-26 23:08:54 +0300

    HT6 T1: Finish task.

---
+++ Home_task_6/Task_1/Task_1/MatrixIterator.cs
@@ -0,0 +1,72 @@
+﻿
+using System.Collections;
+
+namespace Task_1;
+
+internal class MatrixIterator : IEnumerable
+{
+    private readonly int[,] _values;
+    public int GetSize() => _values.GetLength(0);
+    public MatrixIterator(int n)
+    {
+        _values = new int[n,n];
+    }
+
+    public MatrixIterator(int[,] values)
+    {
+        _values = (values.Clone() as int[,])!;
+    }
+
+    public int this[int i, int j]
+    {
+        get { return _values[i,j]; }
+        set { _values[i, j] = value; }
+    }
+
+    public void FillRandom(int minValue, int maxValue)
+    {
+        Random random = new();
+        for (int i = 0; i < _values.GetLength(0); i++)
+        {
+            for (int j = 0; j < _values.GetLength(1); j++)
+            {
+                _values[i, j] = random.Next(minValue, maxValue);
+            }
+        }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        int elementNumber = 1;
+        int diagonalNumber = 0;
+        int dir = 1;
+        int n = _values.GetLength(0);
+        while (elementNumber <= n * n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i + j) == diagonalNumber)
+                    {
+                        if (dir == 1) 
+                        {
+                            elementNumber++;
+                            yield return _values[i, j];
+                        }
+                        else
+                        {
+                            elementNumber++;
+                            yield return _values[j, i];
+                        }
+                    }
+                }
+            }
+            diagonalNumber++;
+            dir = -dir;
+        }
+    }
+}
+
+
+
---
+++ Home_task_6/Task_1/Task_1/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task_1;
+
+
+int[,] ints = { {1,3,4,10,11 }, {2,5,9,12,19 }, {6,8,13,18,20 }, { 7,14,17,21,24}, {15,16,22,23,25 } };
+
+MatrixIterator matrix = new(ints);
+
+ints[0,0] = 77777; //HAS NO EFFECT FOR matrix class array !!!
+
+//Print original matrix
+Console.WriteLine("Print original matrix");
+for (int i = 0; i < ints.GetLength(0); i++)
+{
+    for(int j = 0; j < ints.GetLength(0); j++)
+    {
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine(  );
+}
+Console.WriteLine("\n");
+int counter = 1;
+
+//Print enumerated matrix
+Console.WriteLine("Print enumerated matrix");
+foreach (var item in matrix)
+{
+    if(ints.GetLength(0) < counter)
+    {
+        counter = 1;
+        Console.WriteLine();
+    }
+    counter++;
+    Console.Write(item + "\t");
+}
+
+
+//RANDOM FILL CASE
+Console.WriteLine("\n\n\n-------------------");
+Console.WriteLine("RANDOM FILL CASE");
+
+MatrixIterator matrix2 = new(7);
+matrix2.FillRandom(0, 99);
+//Print original matrix
+Console.WriteLine("Print original matrix");
+for (int i = 0; i < matrix2.GetSize(); i++)
+{
+    for (int j = 0; j < matrix2.GetSize(); j++)
+    {
+        Console.Write(matrix2[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("\n");
+
+int counter2 = 1;
+
+//Print enumerated matrix
+Console.WriteLine("Print enumerated matrix");
+foreach (var item in matrix2)
+{
+    if (matrix2.GetSize() < counter2)
+    {
+        counter2 = 1;
+        Console.WriteLine();
+    }
+    counter2++;
+    Console.Write(item + "\t");
+}

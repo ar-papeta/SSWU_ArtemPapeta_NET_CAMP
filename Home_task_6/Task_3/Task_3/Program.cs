20839fe55a939b5a8d30b091ac21f9b1b3497126
Artem Papeta <ar.papeta@gmail.com>
2023-04-28 21:10:34 +0300

    HT6 T3: Finish task

---
+++ Home_task_6/Task_3/Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task_3;
+
+string text = "Sasha was walking along the highway and eating dried fruit\tCan\ti\ttake\tdried\tfruit";
+foreach (var word in text.UniqueWords())
+{
+    Console.WriteLine(word);
+}
+
+
---
+++ Home_task_6/Task_3/Task_3/StringExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3;
+
+public static class StringExt
+{
+    public static char[] TextSeparators = {' ', '\t' };
+    public static IEnumerable<string> UniqueWords(this string text)
+    {
+        return text.Split(TextSeparators, StringSplitOptions.RemoveEmptyEntries).GroupBy(x => x).Where(x => x.Count() == 1).Select(i => i.Key);
+        // yield return інкапсульовано в linq вираз
+
+        //Не має абсолютно ніякого сенсу (робимо одне і теж 2 рази)
+        //Але в умові задачі треба використати  yield return
+        //тому залишу це тут :)
+        //foreach (var word in words) 
+        //{
+        //    yield return word;
+        //}
+    }
+        
+}

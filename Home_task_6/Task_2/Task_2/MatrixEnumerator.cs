c7fbf988d54bc0d2c42a8413728636f65e865886
Artem Papeta <ar.papeta@gmail.com>
2023-04-28 20:17:03 +0300

    HT6 T2: Finish task.

---
+++ Home_task_6/Task_2/Task_2/MatrixEnumerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2;
+
+internal static class MatrixEnumerator 
+{
+    public static IEnumerable<int> EnumerateMatrix(params int[][] nums)
+    {
+        if(nums is null || nums.Length == 0)
+        {
+            yield break;
+        }
+        var resArr = nums.Aggregate((arr, arrNext) => arr.Concat(arrNext).Order().ToArray());
+        for (int i = 0; i < resArr.Length; i++)
+        {
+            yield return resArr[i];
+        }
+        
+    }
+}
---
+++ Home_task_6/Task_2/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Task_2;
+
+var matrix = MatrixEnumerator.EnumerateMatrix(
+    new[] { -100, 1, 2, 3, 4, 5, 6 },
+    new[] { 11, 2, 23, 4, 5, 36 },
+    new[] { 10, 22, 33, 44, 55, 6, 777 },
+    new int[] {  },
+    new[] { 1000 }
+
+    );
+foreach (var item in matrix)
+{
+    Console.Write($"{item}  ");
+}
+Console.WriteLine("\n");
+
+
+

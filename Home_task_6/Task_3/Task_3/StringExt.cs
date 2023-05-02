2823dda2bdf95992da7d326c822d12b330ab89eb
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-02 12:41:49 +0300

    Update StringExt.cs

--- Home_task_6/Task_3/Task_3/StringExt.cs
+++ Home_task_6/Task_3/Task_3/StringExt.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Task_3;
-
+// сумарний бал 92.
 public static class StringExt
 {
     public static char[] TextSeparators = {' ', '\t' };

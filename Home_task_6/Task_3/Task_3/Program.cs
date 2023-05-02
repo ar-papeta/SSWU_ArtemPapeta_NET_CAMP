06b0c263cd38881bd34c4c30f09b8630ff6c4f39
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-02 12:34:59 +0300

    Update Program.cs

--- Home_task_6/Task_3/Task_3/Program.cs
+++ Home_task_6/Task_3/Task_3/Program.cs
@@ -2,6 +2,7 @@
 using Task_3;
 
 string text = "Sasha was walking along the highway and eating dried fruit\tCan\ti\ttake\tdried\tfruit";
+//Точно треба 1 раз визначити перед циклом UniqueWords()
 foreach (var word in text.UniqueWords())
 {
     Console.WriteLine(word);

3f3d405a89f02598ab69b6f106280f925d359d45
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-16 11:08:12 +0300

    Update IView.cs

--- Home_task_7/Task_1/Task_1/View/IView.cs
+++ Home_task_7/Task_1/Task_1/View/IView.cs
@@ -8,7 +8,7 @@ using Task_1.Models;
 namespace Task_1.View;
 
 public interface IView
-{
+{// Я б називала в інтерфейсі Show, а не Print
     public event Func<(string name, TrafficLightState state)[]> PrintInfoNotify;
     public void PrintInfo(int time);
 }
